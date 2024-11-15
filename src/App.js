1353122f68ab41bc5d9ad7e9e065574314a94fdb
Steven Alexander <alexgrant200@yahoo.com>
2024-11-15 09:07:11 +0700

    feature

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Profiler } from "react";
+import LazyLoad from "react-lazyload";
 
-function App() {
+function LazyImageComponent() {
+  const giganticArray = Array.from({ length: 1000 }, (_, index) => 1 + index);
+
+  function onRender(
+    id,
+    phase,
+    actualDuration,
+    baseDuration,
+    startTime,
+    commitTime
+  ) {
+    console.log("Actual Duration " + actualDuration);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Profiler id="app" onRender={onRender}>
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          gap: "20px",
+        }}
+      >
+        {giganticArray.map((index) => (
+          <LazyLoad height={720} key={index} placeholder="LOADING...">
+            <img
+              key={`huhu-${index}`}
+              src={`https://loremflickr.com/1280/720?random=${index}`} // Append ?random=index to make images unique
+              alt={`Lazy loaded ${index}`}
+              width="100%"
+              height="auto"
+            />
+          </LazyLoad>
+        ))}
+      </div>
+    </Profiler>
   );
 }
 
-export default App;
+export default LazyImageComponent;
