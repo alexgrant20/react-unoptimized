e1141fac791082b92965c7133266238166c8f9a4
Steven Alexander <alexgrant200@yahoo.com>
2024-11-15 08:36:56 +0700

    feature

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { Profiler, useMemo, useState } from "react";
 
-function App() {
+export default function App() {
+  const [count, setCount] = useState(0);
+  const [input, setInput] = useState("");
+
+  function onRender(
+    id,
+    phase,
+    actualDuration,
+    baseDuration,
+    startTime,
+    commitTime
+  ) {
+    console.log("Actual Duration " + actualDuration);
+  }
+
+  const expensiveComputation = (count) => {
+    console.log("Running expensive computation...");
+    let sum = 0;
+    for (let i = 0; i < 100_000_000; i++) {
+      sum += i;
+    }
+    return sum + count;
+  };
+
+  const totalCount = useMemo(expensiveComputation(count), [count]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Profiler id="app" onRender={onRender}>
+      <div className="App">
+        <div>
+          <h2>Memoized Expensive Computation</h2>
+          <p>Count: {count}</p>
+          <p>Computed Result: {totalCount}</p>
+
+          <button onClick={() => setCount(count + 1)}>Increment Count</button>
+
+          <input
+            type="text"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder="Type something..."
+          />
+        </div>
+      </div>
+    </Profiler>
   );
 }
-
-export default App;
